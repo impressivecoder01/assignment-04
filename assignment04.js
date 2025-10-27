38d7dfb11d4061a611a083e96ff589131ea36b6b
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-28 02:03:49 +0600

    pblm2done

--- assignment04.js
+++ assignment04.js
@@ -1,6 +1,13 @@
 // Problem-01 : Train TT's Fine Calculator 
 
+// const f = '6';
 function totalFine( fare ) {
-          // You have to write your code here
+    if(typeof fare === 'number' && fare >=1){
+            const fine = fare + fare*(20/100) + 30;
+            return fine;
+    }
+    else{
+        return 'Invalid'
+    }
 }
-
+// console.log(totalFine(f))
